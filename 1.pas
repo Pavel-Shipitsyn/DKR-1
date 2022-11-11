7a3114b378369cb095e99a5c09ae9e8d587a92da
Pavel-Shipitsyn <117993255+Pavel-Shipitsyn@users.noreply.github.com>
2022-11-11 16:22:45 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,9 @@
+﻿Program z_1;
+var x,y: real;
+begin
+  write('x=');
+  readln(x);
+  if x<-9 then writeln('y=',60/(ln(x)/ln(10)));
+    if (x>=-9) and (x<2) then  writeln('y=',(x*x*x)*(sin(x)/cos(x)));
+    if x>=2 then writeln ('y=',(sin(x)/cos(2*x))*(cos(2*x)/(ln(x)/ln(10))));
+end.
---
+++ 2.pas
@@ -0,0 +1,18 @@
+﻿program Z_2;
+const h=0.1;
+var x:real; a,b:integer;
+begin
+  write('a=');readln(a);
+  write('b=');readln(b);
+  
+
+x:=a;
+while x<=b do
+ begin
+  write('x=',x:2:2);
+  if x<-9 then writeln(' y=',60/(ln(x)/ln(10)):2:2)
+  else if (x>=-9) and (x<2) then writeln(' y=',(x*x*x)*(sin(x)/cos(x)):2:2)
+  else writeln(' y=',(sin(x)/cos(2*x))*(cos(2*x)/(ln(x)/ln(10))):2:2);
+  x:=x+h;
+ end;
+end.
